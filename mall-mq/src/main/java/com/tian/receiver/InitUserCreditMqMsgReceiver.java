63f7d248e512ee317dafd415dcb4ddfbd439ca40
TianWeiChang <603060971@qq.com>
2022-12-11 20:41:42 +0800

    方法命名修改

--- mall-mq/src/main/java/com/tian/receiver/InitUserCreditMqMsgReceiver.java
+++ mall-mq/src/main/java/com/tian/receiver/InitUserCreditMqMsgReceiver.java
@@ -1,29 +1,26 @@
 package com.tian.receiver;
 
-import com.rabbitmq.client.Channel;
 import com.tian.dto.UserCreditDto;
 import com.tian.mq.RabbitMQConstants;
 import com.tian.service.UserCreditDubboService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.util.Arrays;
-import java.util.Map;
 
 /**
  * @author tianwc  公众号：java后端技术全栈、面试专栏
  * @version 1.0.0
  * @date 2022年12月11日 15:34
+ * <p>
+ * 初始化用户积分 消息处理
  */
 @Component
 @RabbitListener(queues = RabbitMQConstants.INIT_USER_CREDIT_QUEUE)
 public class InitUserCreditMqMsgReceiver {
+
     private Logger logger = LoggerFactory.getLogger(MQMsgReceiver.class);
 
     @DubboReference(version = "1.0.0", check = false)
